0e9a0d93cb3a5401e477395da20afe080cdc686d
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-02-04 19:25:16 +0600

    first added

---
+++ first.js
@@ -0,0 +1,9 @@
+
+let number = [56, 78, 765, 45, 34, 23, 98, 99, 78, 89, 85, 93, 80];
+for (let i = 0; i < number.length; i++) {
+   let element = number[i];
+   if (element >= 80) {
+      console.log(element);
+   }
+
+}
